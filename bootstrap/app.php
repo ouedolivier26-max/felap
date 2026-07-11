6a17ac02d628414c9a6413818748119c956455e9
ouedolivier26-max <ouedolivier26@gmail.com>
2026-07-11 12:49:29 +0000

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,15 @@ return Application::configure(basePath: dirname(__DIR__))
             'IsLivreur' => IsLivreur::class,
             'IsAdmin'   => IsAdmin::class,
         ]);
+
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+                | Request::HEADER_X_FORWARDED_AWS_ELB,
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
